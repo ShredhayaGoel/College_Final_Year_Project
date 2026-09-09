d20940ca475943d23819b8a5b32ef45e50083e59
Shredhaya <shredhayagoel@gmail.com>
2026-09-09 06:20:23 +0530

    added packages

--- event_management/vite.config.js
+++ event_management/vite.config.js
@@ -4,7 +4,8 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     svgr(),
+    // tailwindcss()
   ],
 })
