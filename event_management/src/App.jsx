17f28f3eefda06ee57dda62236776e678dae08eb
Shredhaya <shredhayagoel@gmail.com>
2026-09-09 06:21:16 +0530

    create routes for login , signup, forgotpassword , new password

--- event_management/src/App.jsx
+++ event_management/src/App.jsx
@@ -12,6 +12,9 @@ import Contact from "./components/Contact";
 import UpcomingEvent from "./components/upcomingEvents/UpcomingEvent";
 
 import EventDetails from "./pages/eventdetails";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/Forgotpassword";
+import NewPassword from "./pages/Newpassword";
 
 function App() {
   return (
@@ -32,9 +35,11 @@ function App() {
           />
 
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/newpassword" element={<NewPassword />} />
 
           <Route path="/events/:id" element={<EventDetails />} />
-          
         </Routes>
       </BrowserRouter>
     </>
